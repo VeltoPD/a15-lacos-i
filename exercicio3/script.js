cc305e4a5ecc55c71ecbb5869520c2b184d98c88
VeltoPD <veltopd@gmail.com>
2022-09-12 19:43:40 -0300

    Entrega dos exercícios a015

---
+++ exercicio3/script.js
@@ -0,0 +1,4 @@
+const tabelaAtual = ["Palmeiras","Internacional","Fluminense","Corinthians","Atlético Mineiro"]
+for (let i = 0; i < tabelaAtual.length; i++) {
+    console.log(`${i + 1} - ${tabelaAtual[i]}`)
+}
